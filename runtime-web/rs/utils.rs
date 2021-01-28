2e06e0f8c8ad8529817ca83cf179c007d9a93fc7
kazatsuyu <shirayama.kazatsuyu@gmail.com>
2021-01-28 23:31:36 +0900

    setTimeoutによる副作用がpollされるまで発生しないよう変更

--- runtime-web/rs/utils.rs
+++ runtime-web/rs/utils.rs
@@ -1,5 +1,5 @@
+// When the `console_error_panic_hook` feature is enabled, we can call the
 pub fn set_panic_hook() {
-    // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function at least once during initialization, and then
     // we will get better error messages if our code ever panics.
     //
--- vdom/src/timer.rs
+++ vdom/src/timer.rs
@@ -8,7 +8,7 @@ use std::{
 #[cfg(target_arch = "wasm32")]
 use {
     crate::cmd::get_trriger,
-    std::{cell::Cell, rc::Rc},
+    std::{cell::Cell, cell::RefCell, mem::replace, rc::Rc},
     wasm_bindgen::prelude::Closure,
     wasm_bindgen::JsCast,
     web_sys::window,
@@ -23,34 +23,55 @@ where
     F: 'static + Fn() -> Msg,
     Msg: 'static,
 {
-    struct Timeout(Rc<Cell<bool>>);
-    impl Future for Timeout {
-        type Output = ();
+    struct Timeout<F, Msg>
+    where
+        F: 'static + Fn() -> Msg,
+        Msg: 'static,
+    {
+        ready: Rc<Cell<bool>>,
+        trigger: Option<Rc<RefCell<dyn FnMut()>>>,
+        closure: Option<Closure<dyn FnMut()>>,
+        ms: i32,
+        f: F,
+    }
+    impl<F, Msg> Future for Timeout<F, Msg>
+    where
+        F: 'static + Fn() -> Msg,
+        Msg: 'static,
+    {
+        type Output = Msg;
         fn poll(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Self::Output> {
-            if self.0.get() {
-                Poll::Ready(())
+            let this = unsafe { Pin::get_unchecked_mut(self) };
+            if this.ready.get() {
+                Poll::Ready((this.f)())
             } else {
+                if let Some(trigger) = replace(&mut this.trigger, None) {
+                    let weak = Rc::downgrade(&this.ready);
+                    let closure = Closure::once(Box::new(move || {
+                        if let Some(timeout) = weak.upgrade() {
+                            timeout.set(true);
+                            trigger.borrow_mut()();
+                        }
+                    }));
+                    window()
+                        .unwrap()
+                        .set_timeout_with_callback_and_timeout_and_arguments_0(
+                            closure.as_ref().unchecked_ref(),
+                            this.ms,
+                        )
+                        .unwrap();
+                    this.closure = Some(closure);
+                }
                 Poll::Pending
             }
         }
     }
-    let timeout = Timeout(Rc::new(Cell::new(false)));
-    let weak = Rc::downgrade(&timeout.0);
-    let trigger = get_trriger();
-    let closure = Closure::<dyn FnOnce()>::once(Box::new(move || {
-        if let Some(timeout) = weak.upgrade() {
-            timeout.set(true);
-            trigger.borrow_mut()();
-        }
-    }));
-    window()
-        .unwrap()
-        .set_timeout_with_callback_and_timeout_and_arguments_0(closure.as_ref().unchecked_ref(), ms)
-        .unwrap();
-
-    Cmd::promise(timeout).map(move |()| {
-        &closure;
-        f()
+    Cmd::promise(Timeout {
+        ready: Rc::new(Cell::new(false)),
+        trigger: Some(get_trriger()),
+        closure: None,
+        ms,
+        f,
     })
 }
 
